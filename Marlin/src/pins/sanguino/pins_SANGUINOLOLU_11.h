bfbc01a94ae75481492d0898d43208fa73ba2229
Brian MacIntosh <brianamacintosh@gmail.com>
2021-12-26 13:49:26 -0800

    undef FAN_PIN so 24V can always be picked up from fan headers

--- Marlin/src/pins/sanguino/pins_SANGUINOLOLU_11.h
+++ Marlin/src/pins/sanguino/pins_SANGUINOLOLU_11.h
@@ -99,7 +99,7 @@
   #define E0_ENABLE_PIN                       14
 
   #if !defined(FAN_PIN) && ENABLED(LCD_I2C_PANELOLU2)
-    #define FAN_PIN                            4  // Uses Transistor1 (PWM) on Panelolu2's Sanguino Adapter Board to drive the fan
+    //#define FAN_PIN                            4  // Uses Transistor1 (PWM) on Panelolu2's Sanguino Adapter Board to drive the fan
   #endif
 
 #else
@@ -113,7 +113,7 @@
 #endif
 
 #if !defined(FAN_PIN) && (MB(AZTEEG_X1, STB_11) || IS_MELZI)
-  #define FAN_PIN                              4  // Works for Panelolu2 too
+  //#define FAN_PIN                              4  // Works for Panelolu2 too
 #endif
 
 //
